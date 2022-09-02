88adab8150dd9a08c9a1742f39b624fdd2fe1a64
Taras <tarasl@ven.com>
2022-09-02 22:42:44 +0300

    Final TEst Cases

--- src/main/java/StudentDashboardAssignment.java
+++ src/main/java/StudentDashboardAssignment.java
@@ -1,35 +1,18 @@
 import com.fasterxml.jackson.annotation.JsonProperty;
 
-public class StudentDashboardAssignment extends AssignmentDataForStudent {
+public class StudentDashboardAssignment {
     @JsonProperty
     public Integer id;
+
+    @JsonProperty("student_id")
+    public Integer studentId;
+
+    @JsonProperty("content_id")
+    public Integer contentId;
 
     @JsonProperty
     public String solution;
 
     @JsonProperty
     public Integer mark;
-
-    public StudentDashboardAssignment(String targetType, Integer studentId, Integer contentId) {
-        super(targetType, studentId, contentId);
-    }
-
-    public void AssigmentDashboardForStudent(Integer id, String solution, Integer mark, String targetType, Integer studentId, Integer contentId) {
-        this.id = id;
-        this.solution = solution;
-        this.mark = mark;
-    }
-
-
-
-    @Override
-    public String toString() {
-        return "Assignment{" +
-                "id=" + id +
-                ", solution='" + solution + '\'' +
-                ", mark=" + mark +
-                ", student_id=" + studentId +
-                ", content_id=" + contentId +
-                '}';
-    }
 }
--- src/test/java/ApiTests.java
+++ src/test/java/ApiTests.java
@@ -1,7 +1,10 @@
 import io.restassured.RestAssured;
 import io.restassured.response.Response;
+import org.json.JSONObject;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+
+import java.lang.reflect.Type;
 import java.util.*;
 import java.util.stream.Collectors;
 
@@ -44,6 +47,7 @@ public class ApiTests extends BaseTest {
         Assert.assertEquals(allStudents.length, 3);
         //    System.out.println(Arrays.asList(allStudents));
     }
+
     @Test
     public void testCreateAndUpdateStudent() {
         Response stud4CreateResponse = RestAssured.given()
@@ -78,6 +82,7 @@ public class ApiTests extends BaseTest {
         Set<String> names = Arrays.stream(searchResult).map(s -> s.firstName).collect(Collectors.toSet());
         Assert.assertEquals(names, Set.of("Sara"));
     }
+
     @Test
     public void testSearchStudentByLastName() {
         Response response = RestAssured.given()
@@ -95,6 +100,7 @@ public class ApiTests extends BaseTest {
         Student[] allStudents = RestAssured.get("/students").as(Student[].class);
         Assert.assertEquals(allStudents.length, 4);
     }
+
     @Test
     public void createNewGroup() {
         List<Student> allStudents = Arrays.asList(
@@ -114,6 +120,7 @@ public class ApiTests extends BaseTest {
         //    System.out.println(group.name);
         //    System.out.println(group.students);
     }
+
     @Test
     public void createJobTemplate() {
         Response createJobTemplate = RestAssured.given()
@@ -123,64 +130,176 @@ public class ApiTests extends BaseTest {
         AssignmentContent content = createJobTemplate.as(AssignmentContent.class);
         Assert.assertEquals(content.content, "CreateApiTest");
     }
+
     @Test
-    public void assignmentForStudents() {
+    public void zassignmentForStudents() {
         Integer student;
         Integer content;
 
-        //Students creation
-        Response stud1CreateResponse = RestAssured.given()
-                .body(new StudentData("Mike", "Milann"))
-                .post("/students");
-        stud1CreateResponse.then().statusCode(200);
-        Student stud1 = stud1CreateResponse.as(Student.class);
-        Response stud1GetResponse = RestAssured.get("/students/{id}", stud1.id);
-        Student receivedStud1 = stud1GetResponse.as(Student.class);
-        Assert.assertEquals(receivedStud1.firstName, "Mike");
-        Assert.assertEquals(receivedStud1.lastName, "Milann");
-        // Student_2
-        Response stud2CreateResponse = RestAssured.given()
-                .body(new StudentData("Ella", "Maven"))
-                .post("/students");
-        stud2CreateResponse.then().statusCode(200);
-        Student stud2 = stud2CreateResponse.as(Student.class);
-        Response st2GetResponse = RestAssured.get("/students/{id}", stud2.id);
-        Student receivedStud2 = st2GetResponse.as(Student.class);
-        Assert.assertEquals(receivedStud2.firstName, "Ella");
-        Assert.assertEquals(receivedStud2.lastName, "Maven");
-        // Student_3
-        Response stud3CreateResponse = RestAssured.given()
-                .body(new StudentData("Sara", "Ganen"))
-                .post("/students");
-        stud3CreateResponse.then().statusCode(200);
-        Student stud3 = stud3CreateResponse.as(Student.class);
-        Response stud3GetResponse = RestAssured.get("/students/{id}", stud3.id);
-        Student receivedStud3 = stud3GetResponse.as(Student.class);
-        Assert.assertEquals(receivedStud3.firstName, "Sara");
-        Assert.assertEquals(receivedStud3.lastName, "Ganen");
-
-        //Assigning tasks
+        //Назначение заданий и загрузка решения к нему
         Student[] getAllStudents = RestAssured.get("/students").as(Student[].class);
-        Assert.assertEquals(getAllStudents.length, 3);
+        Assert.assertEquals(getAllStudents.length, 6);
         RestAssured.given()
                 .body(new AssignmentContent("CreateApiTest"))
                 .post("/content")
                 .then().statusCode(200);
         AssignmentContent[] allAssignmentContent = RestAssured.get("/content").as(AssignmentContent[].class);
-        int start_process = 0;
-        int end_process = getAllStudents.length / 2;
-        for (int i = 0; i < allAssignmentContent.length; i++) {
-            if (i == 1) {start_process = getAllStudents.length / 2; end_process = getAllStudents.length;}
-            for (int s = start_process; s < end_process; s++) {
-                content = allAssignmentContent[i].id;
-                student = getAllStudents[s].id;
-                StudentDashboardAssignment assignmentForStudent = RestAssured.given()
-                        .body(new AssignmentDataForStudent("student", student, content))
-                        .post("/assignments")
+        for (int i = 0; i < getAllStudents.length; i++) {
+            content = allAssignmentContent[0].id;
+            student = getAllStudents[i].id;
+
+
+            JSONObject joAssignmPost = new JSONObject();
+            joAssignmPost.put("target_type", "student");
+            joAssignmPost.put("student_id", student);
+            joAssignmPost.put("content_id", content);
+
+            StudentDashboardAssignment assignmentForStudent = RestAssured.given()
+                    .body(joAssignmPost.toString())
+                    .post("/assignments")
+                    .then().statusCode(200)
+                    .extract().as(StudentDashboardAssignment.class);
+
+//            System.out.println(assignmentForStudent.solution);
+
+            Assert.assertEquals(assignmentForStudent.studentId, student);
+            Assert.assertEquals(assignmentForStudent.contentId, content);
+
+            JSONObject solutionPost = new JSONObject();
+            solutionPost.put("solution", "Test");
+
+            StudentSolution solutionForStudent = RestAssured.given()
+                    .body(solutionPost.toString())
+                    .post("assignments/{id}/solution", assignmentForStudent.id.toString())
+                    .then().statusCode(200)
+                    .extract().as(StudentSolution.class);
+
+            // Простановка оценки для задания
+            JSONObject markPost = new JSONObject();
+            markPost.put("mark", 5);
+
+            StudentSolution markForStudent = RestAssured.given()
+                    .body(markPost.toString())
+                    .post("assignments/{id}/mark", assignmentForStudent.id.toString())
+                    .then().statusCode(200)
+                    .extract().as(StudentSolution.class);
+
+        }
+    }
+
+    @Test
+    public void zassi1gnmentForStudents() {
+        Integer student;
+        Integer content;
+        Response stud1CreateResponse = RestAssured.given()
+                .body(new StudentData("Zina", "Sory"))
+                .post("/students");
+        stud1CreateResponse.then().statusCode(200);
+        Student stud1 = stud1CreateResponse.as(Student.class);
+        Response stud1GetResponse = RestAssured.get("/students/{id}", stud1.id);
+        Student receivedStud1 = stud1GetResponse.as(Student.class);
+        Assert.assertEquals(receivedStud1.firstName, "Zina");
+        // Student_2
+        Response stud2CreateResponse = RestAssured.given()
+                .body(new StudentData("Gor", "Ramen"))
+                .post("/students");
+        stud2CreateResponse.then().statusCode(200);
+        Student stud2 = stud2CreateResponse.as(Student.class);
+        Response st2GetResponse = RestAssured.get("/students/{id}", stud2.id);
+        Student receivedStud2 = st2GetResponse.as(Student.class);
+        Assert.assertEquals(receivedStud2.firstName, "Gor");
+        Assert.assertEquals(receivedStud2.lastName, "Ramen");
+
+        //Assigning tasks
+        Student[] getAllStudents = RestAssured.get("/students").as(Student[].class);
+        Assert.assertEquals(getAllStudents.length, 6);
+        RestAssured.given()
+                .body(new AssignmentContent("CreateApiTest"))
+                .post("/content")
+                .then().statusCode(200);
+        AssignmentContent[] allAssignmentContent = RestAssured.get("/content").as(AssignmentContent[].class);
+
+        for (int i = 0; i < getAllStudents.length; i++) {
+            content = allAssignmentContent[0].id;
+            student = getAllStudents[i].id;
+
+            JSONObject joAssignmPost = new JSONObject();
+            joAssignmPost.put("target_type", "student");
+            joAssignmPost.put("student_id", student);
+            joAssignmPost.put("content_id", content);
+
+            StudentDashboardAssignment assignmentForStudent = RestAssured.given()
+                    .body(joAssignmPost.toString())
+                    .post("/assignments")
+                    .then().statusCode(200)
+                    .extract().as(StudentDashboardAssignment.class);
+
+//            System.out.println(assignmentForStudent.solution);
+
+            Assert.assertEquals(assignmentForStudent.studentId, student);
+            Assert.assertEquals(assignmentForStudent.contentId, content);
+
+            // Автоматическое оценивание заданий без оценок (проверить что задания, у которых есть решение, получат 5, а задания без решения получат 1)
+            if (getAllStudents[i].firstName.equals("Gor")) {
+                JSONObject solutionPost = new JSONObject();
+                solutionPost.put("solution", "Test");
+
+                StudentSolution solutionForStudent = RestAssured.given()
+                        .body(solutionPost.toString())
+                        .post("assignments/{id}/solution", assignmentForStudent.id.toString())
                         .then().statusCode(200)
-                        .extract().as(StudentDashboardAssignment.class);
-                Assert.assertEquals(assignmentForStudent.studentId, student);
-                Assert.assertEquals(assignmentForStudent.contentId, content);
+                        .extract().as(StudentSolution.class);
+
+                JSONObject markPost = new JSONObject();
+                markPost.put("mark", 5);
+
+                StudentSolution markForStudent = RestAssured.given()
+                        .body(markPost.toString())
+                        .post("assignments/{id}/mark", assignmentForStudent.id.toString())
+                        .then().statusCode(200)
+                        .extract().as(StudentSolution.class);
+
+                for (int y = 0; y < getAllStudents.length; y++) {
+
+                    // Проверить, что автоматическое оценивание не затрагивает уже выставленные оценки
+                    if (markForStudent.solution == null && markForStudent.mark == null) {
+                        content = allAssignmentContent[0].id;
+                        student = getAllStudents[i].id;
+
+                        JSONObject joAssignmPostRepeat = new JSONObject();
+                        joAssignmPost.put("target_type", "student");
+                        joAssignmPost.put("student_id", student);
+                        joAssignmPost.put("content_id", content);
+
+                        StudentDashboardAssignment assignmentForStudentRepeat = RestAssured.given()
+                                .body(joAssignmPost.toString())
+                                .post("/assignments")
+                                .then().statusCode(200)
+                                .extract().as(StudentDashboardAssignment.class);
+
+                        Assert.assertNull(assignmentForStudent.mark);
+                    }
+                    else if (markForStudent.mark == null) {
+
+                        int oldMark = assignmentForStudent.mark;
+
+                        content = allAssignmentContent[0].id;
+                        student = getAllStudents[i].id;
+
+                        JSONObject joAssignmPostRepeat = new JSONObject();
+                        joAssignmPost.put("target_type", "student");
+                        joAssignmPost.put("student_id", student);
+                        joAssignmPost.put("content_id", content);
+
+                        StudentDashboardAssignment assignmentForStudentRepeat = RestAssured.given()
+                                .body(joAssignmPost.toString())
+                                .post("/assignments")
+                                .then().statusCode(200)
+                                .extract().as(StudentDashboardAssignment.class);
+
+                        Assert.assertEquals(assignmentForStudent.mark, oldMark);
+                    }
+                }
             }
         }
     }
